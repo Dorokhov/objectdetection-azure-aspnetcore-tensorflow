c65d67433c1cfe355fecb46f05ce4b4413877281
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-01 14:54:16 +0300

    assets

---
+++ Deploy/assets/launch.sh
@@ -0,0 +1,21 @@
+cd /home/testadmin/
+sudo git clone "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow"
+cd ./objectdetection/
+sudo dotnet publish
+cd /bin/Debug/netcoreapp2.0
+
+sudo cp -a ./bin/Debug/netcoreapp2.0 /home/testadmin/temp
+sudo rm /home/testadmin/objectdetection/*
+sudo cp -a /home/testadmin/temp /home/testadmin/objectdetection/
+
+wget
+
+sudo apt-get -y install nginx
+sudo service nginx start
+
+cd /etc/nginx/
+
+sudo nginx -t 
+sudo nginx -s reload
+
+sudo apt-get -y install supervisor
