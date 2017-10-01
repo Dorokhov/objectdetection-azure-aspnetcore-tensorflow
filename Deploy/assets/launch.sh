f9f7c87e9fbd1cc0b6d421191dd8a601b3d2a279
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-01 19:14:31 +0300

    launch

--- Deploy/assets/launch.sh
+++ Deploy/assets/launch.sh
@@ -2,18 +2,18 @@ cd /home/testadmin/
 sudo git clone "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow"
 cd ./objectdetection/
 sudo dotnet publish
-cd /bin/Debug/netcoreapp2.0
 
-sudo cp -a ./bin/Debug/netcoreapp2.0 /home/testadmin/temp
-sudo rm /home/testadmin/objectdetection/*
-sudo cp -a /home/testadmin/temp /home/testadmin/objectdetection/
+sudo cp -a ./objectdetection/bin/Debug/netcoreapp2.0/publish /home/testadmin/temp
+sudo rm -r /home/testadmin/objectdetection/*
+sudo cp -a /home/testadmin/temp/* /home/testadmin/objectdetection/
 
 wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/libtensorflow.so"
 
 sudo apt-get -y install nginx
 sudo service nginx start
 
-cd /etc/nginx/
+cd /etc/nginx/sites-available/
+wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/default"
 
 sudo nginx -t 
 sudo nginx -s reload
