1118417d70694771609d9bba5e3d5f9b01d44bc0
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-07 19:32:31 +0300

    Update install_xrdp.sh

--- Deploy/install_xrdp.sh
+++ Deploy/install_xrdp.sh
@@ -2,15 +2,6 @@
 sudo apt-get update
 #install gnome desktop
 #sudo apt-get install ubuntu-desktop -y
-#install xrdp
-sudo apt-get install xrdp -y
-
-#using xfce if you are using Ubuntu version later than Ubuntu 12.04LTS
-sudo apt-get install xfce4 -y
-#sudo apt-get install xubuntu-desktop -y
-sudo echo xfce4-session >/root/.xsession
-sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
-sudo service xrdp restart
 
 #text editor
 #sudo apt-get -y install cream
@@ -38,3 +29,13 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsof
 
 sudo apt-get update
 sudo apt-get -y install dotnet-sdk-2.0.0
+
+#install xrdp
+sudo apt-get install xrdp -y
+
+#using xfce if you are using Ubuntu version later than Ubuntu 12.04LTS
+sudo apt-get install xfce4 -y
+#sudo apt-get install xubuntu-desktop -y
+sudo echo xfce4-session >/root/.xsession
+sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
+sudo service xrdp restart
