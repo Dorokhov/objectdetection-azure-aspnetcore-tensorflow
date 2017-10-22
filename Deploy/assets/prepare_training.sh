07717584ea7acffb0daedd7ac8a088e466e79fd0
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-22 19:02:49 +0300

    prepare training

---
+++ Deploy/assets/prepare_training.sh
@@ -0,0 +1,33 @@
+sudo pip3 install pillow
+sudo pip3 install lxml
+
+sudo apt-get -y install unzip
+sudo apt-get -y install protobuf-compiler python-pil python-lxml
+sudo pip install jupyter
+sudo pip install matplotlib
+
+cd /usr/local/lib/python3.4/dist-packages/tensorflow/
+sudo mkdir protoc_3.3
+cd protoc_3.3
+sudo wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip
+sudo chmod 775 protoc-3.3.0-linux-x86_64.zip
+sudo unzip protoc-3.3.0-linux-x86_64.zip
+
+
+cd /home/testadmin/
+mkdir training
+cd training
+git clone https://github.com/tensorflow/models
+cd models/research
+/usr/local/lib/python3.4/dist-packages/tensorflow/protoc_3.3/bin/protoc object_detection/protos/*.proto --python_out=.
+
+cd /home/testadmin/training/models/research
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+sudo wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+tar -xvf annotations.tar.gz
+tar -xvf images.tar.gz
+python3 object_detection/create_pet_tf_record.py --label_map_path=object_detection/data/pet_label_map.pbtxt --data_dir=`pwd` --output_dir=`pwd`
+
+#python object_detection/train.py --logtostderr --pipeline_config_path=object_detection/samples/configs/ssd_mobilenet_v1_pets.config --train_dir=train
--- Deploy/install_objdetection.sh
+++ Deploy/install_objdetection.sh
@@ -1,2 +1,3 @@
 bash install_xrdp.sh
 bash launch.sh
+bash prepare_training.sh
