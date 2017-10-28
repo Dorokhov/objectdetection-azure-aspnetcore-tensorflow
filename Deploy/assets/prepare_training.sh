f99593aa2339cd65eb56368f356e8493fe366edb
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-28 19:34:21 +0300

    ARM Template

--- Deploy/assets/prepare_training.sh
+++ Deploy/assets/prepare_training.sh
@@ -21,6 +21,14 @@ git clone https://github.com/tensorflow/models
 cd models/research
 /usr/local/lib/python3.4/dist-packages/tensorflow/protoc_3.3/bin/protoc object_detection/protos/*.proto --python_out=.
 
+
+cd object_detection/samples/configs
+wget -N "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/ssd_mobilenet_v1_pets.config"
+
+cd home/testadmin
+wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/train.sh"
+wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/start_tensorboard.sh"
+
 cd /home/testadmin/training/models/research
 mkdir train
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
@@ -31,9 +39,3 @@ tar -xvf annotations.tar.gz
 tar -xvf images.tar.gz
 python3 object_detection/create_pet_tf_record.py --label_map_path=object_detection/data/pet_label_map.pbtxt --data_dir=`pwd` --output_dir=`pwd`
 
-cd object_detection/samples/configs
-wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/ssd_mobilenet_v1_pets.config"
-
-cd home/testadmin
-wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/train.sh"
-wget "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/start_tensorboard.sh"
