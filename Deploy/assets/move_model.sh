6f937162309825a1251df93eac290e49e64478a6
Dorokhov <dorokhov.vladimir@gmail.com>
2017-10-29 18:24:31 +0200

    ARM Template

--- Deploy/assets/move_model.sh
+++ Deploy/assets/move_model.sh
@@ -1,6 +1,6 @@
 cd /home/testadmin/training/models/research
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 python3 object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path object_detection/samples/configs/ssd_mobilenet_v1_pets.config --trained_checkpoint_prefix train/model.ckpt-0 --output_directory frozen_inference_graph.pb
-cp -a ./frozen_inference_graph.pb /home/testadmin/objectdetection
+cp -a ./frozen_inference_graph.pb/frozen_inference_graph.pb /home/testadmin/objectdetection
 cd /home/testadmin/objectdetection
 wget -N "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow/raw/master/Deploy/assets/ssd_mobilenet_v1_pets.config"
