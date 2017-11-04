de5c20120dd6e3c692156cf7ef1382bfe1caea75
Dorokhov <dorokhov.vladimir@gmail.com>
2017-11-05 01:10:13 +0200

    gpu

--- Deploy/assets/install_xrdp.sh
+++ Deploy/assets/install_xrdp.sh
@@ -22,8 +22,8 @@ sudo apt-get -y install python-pip python-dev
 #CPU
 #sudo pip install tensorflow
 #GPU
-sudo pip install libcupti-dev
-sudo pip install tensorflow-gpu
+#sudo pip install libcupti-dev
+#sudo pip install tensorflow-gpu
 
 #install dotnet core
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
--- Deploy/assets/launch.sh
+++ Deploy/assets/launch.sh
@@ -1,4 +1,5 @@
 cd /home/testadmin/
+git clone https://github.com/leestott/Azure-GPU-Setup.git
 git clone "https://github.com/Dorokhov/objectdetection-azure-aspnetcore-tensorflow" objectdetection
 cd ./objectdetection/
 sudo dotnet publish
