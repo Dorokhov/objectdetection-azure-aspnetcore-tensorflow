1797f5d2c3e558fb3a4efc734d58585b22f6ddf9
Dorokhov <dorokhov.vladimir@gmail.com>
2017-11-15 14:27:17 +0200

    deploy

--- DeployTraining/assets/eval.sh
+++ DeployTraining/assets/eval.sh
@@ -1,4 +1,4 @@
+. export_variables.sh
 cd $MODELS_PATH
-sh export_variables.sh
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 python3 object_detection/eval.py --logtostderr --pipeline_config_path=train/$CONFIG_NAME --checkpoint_dir=train --eval_dir=eval
--- DeployTraining/assets/train.sh
+++ DeployTraining/assets/train.sh
@@ -1,4 +1,4 @@
+. export_variables.sh
 cd $MODELS_PATH
-sh export_variables.sh
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
-python3 object_detection/train.py --logtostderr --pipeline_config_path=train/$CONFIG_NAME --train_dir=train
+python3 $MODELS_PATH/object_detection/train.py --logtostderr --pipeline_config_path=train/$CONFIG_NAME --train_dir=train
