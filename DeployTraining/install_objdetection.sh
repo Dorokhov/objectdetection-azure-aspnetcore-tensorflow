73baee176b576846eccf4318dee53118dc351096
Dorokhov <dorokhov.vladimir@gmail.com>
2017-11-15 13:22:24 +0200

    deploy

--- DeployTraining/install_objdetection.sh
+++ DeployTraining/install_objdetection.sh
@@ -1,5 +1,4 @@
 bash install_xrdp.sh
-bash launch.sh
 bash prepare_training.sh
 sudo chown -R testadmin:testadmin /home/testadmin/objectdetection
 sudo chown -R testadmin:testadmin /home/testadmin/training
