e4a2ebdc8f9803b7b3112e813995fd3f5fd31879
Dorokhov <dorokhov.vladimir@gmail.com>
2017-11-15 20:07:10 +0200

    deploy

--- Deploy/assets/install_xrdp.sh
+++ Deploy/assets/install_xrdp.sh
@@ -28,7 +28,7 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.0.0
+sudo apt-get -y install dotnet-sdk-2.0.0
 
 #install xrdp
 sudo apt-get install xrdp -y
