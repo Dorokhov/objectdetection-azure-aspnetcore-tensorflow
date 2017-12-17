fdab9940ab7a0192fab9dc2ed41119cbd38fdabf
Dorokhov <dorokhov.vladimir@gmail.com>
2017-12-17 18:48:01 +0200

    deploy

--- DeployTraining/assets/create_record.sh
+++ DeployTraining/assets/create_record.sh
@@ -1,2 +1,5 @@
-python3 object_detection/$CREATE_RECORD_FILE --label_map_path=object_detection/data/$OBJECT_LABELS --data_dir=train/VOCdevkit --year=VOC2012 --set=train --output_path=train/mscoco_train.record
-python3 object_detection/$CREATE_RECORD_FILE --label_map_path=object_detection/data/$OBJECT_LABELS --data_dir=train/VOCdevkit --year=VOC2012 --set=val --output_path=train/mscoco_val.record
+. export_variables.sh
+cd $MODELS_PATH
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+python3 object_detection/dataset_tools/$CREATE_RECORD_FILE --label_map_path=object_detection/data/$OBJECT_LABELS --data_dir=train/VOCdevkit --year=VOC2012 --set=train --output_path=train/mscoco_train.record
+python3 object_detection/dataset_tools/$CREATE_RECORD_FILE --label_map_path=object_detection/data/$OBJECT_LABELS --data_dir=train/VOCdevkit --year=VOC2012 --set=val --output_path=train/mscoco_val.record
